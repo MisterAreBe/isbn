2fa4e67e55fcc7adc6613415a84d25bd6011ad69
Ricky Collins <richardcollins52317@gmail.com>
2018-08-07 09:23:27 -0400

    pass, isbn13 ignore dashes and spaces

--- isbn.rb
+++ isbn.rb
@@ -30,6 +30,7 @@ def isbn_10(num)
 end
 
 def isbn_13(num)
+    num.gsub!(/[- ]/, '')
     if num.length != 13
         return false
     end
@@ -47,3 +48,11 @@ end
 # checksum = 232%11
 # checksum = 1
 # isbn = 7421394761
+
+
+# The check digit for ISBN-13 is calculated by multiplying
+# each digit alternately by 1 or 3 (i.e., 1 x 1st digit,
+# 3 x 2nd digit, 1 x 3rd digit, 3 x 4th digit, etc.), summing
+# these products together, taking modulo 10 of the result
+# and subtracting this value from 10, and then taking the
+# modulo 10 of the result again to produce a single digit.
