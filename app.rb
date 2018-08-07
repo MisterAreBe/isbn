056b1e63ca1440643fc730869196173bc7970fd5
Ricky Collins <richardcollins52317@gmail.com>
2018-08-07 12:55:41 -0400

    added front end

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
+require_relative 'isbn.rb'
 
 enable :sessions
 
 get '/' do
-  erb :index, :layout => :layout 
+  isbn_check = session[:check] || ''
+  erb :layout, locals: {isbn_check: isbn_check}
+end
+
+post '/check_isbn' do
+  isbn = params[:isbn]
+  temp = isbn(isbn)
+  isbn_str = ''
+  if temp == true
+    isbn_str += "Valid"
+  else
+    isbn_str += "Invalid"
+  end
+  session[:check] = isbn_str
+  redirect '/'
 end
