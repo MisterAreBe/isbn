66597ff74aa456b219cd844adcb90865030e21c2
Ricky Collins <richardcollins52317@gmail.com>
2018-08-08 14:12:18 -0400

    Made it possible to keep and store isbns

--- app.rb
+++ app.rb
@@ -4,19 +4,24 @@ require_relative 'isbn.rb'
 enable :sessions
 
 get '/' do
-  isbn_check = session[:check] || ''
-  erb :layout, locals: {isbn_check: isbn_check}
+  isbn_check = session[:check] || []
+  isbn_holder = session[:isbn] || []
+  erb :layout, locals: {isbn_check: isbn_check, isbn_holder: isbn_holder}
 end
 
 post '/check_isbn' do
   isbn = params[:isbn]
-  temp = isbn(isbn)
-  isbn_str = ''
+  stored_isbn = params[:stored_isbn] || []
+  isbn_str = params[:isbn_str] || []
+  stored_isbn << isbn
+  temp = isbn_refa3(isbn)
   if temp == true
-    isbn_str += "Valid"
-  else
-    isbn_str += "Invalid"
+    temp = "Valid"
+  elsif temp == false
+    temp = "Invalid"
   end
+  isbn_str << temp
   session[:check] = isbn_str
+  session[:isbn] = stored_isbn
   redirect '/'
 end
--- isbn.rb
+++ isbn.rb
@@ -1,90 +1,90 @@
-def isbn_10(num)
-    num.gsub!(/[- ]/, '')
-    bad = 0
-    if num.gsub(/[\D]/, '') != num
-        bad += 1
-    end
-    if num.end_with?('x') || num.end_with?('X')
-        bad -= 1
-        check = num.gsub(/[\D]/, '')
-        x = num[-1]
-        if "#{check}#{x}" != num
-            bad += 1
-        end
-    end
+# def isbn_10(num)
+#     num.gsub!(/[- ]/, '')
+#     bad = 0
+#     if num.gsub(/[\D]/, '') != num
+#         bad += 1
+#     end
+#     if num.end_with?('x') || num.end_with?('X')
+#         bad -= 1
+#         check = num.gsub(/[\D]/, '')
+#         x = num[-1]
+#         if "#{check}#{x}" != num
+#             bad += 1
+#         end
+#     end
      
-    unless bad <= 0
-        return false
-    end
+#     unless bad <= 0
+#         return false
+#     end
 
-    temp = num.split('')
-    check_digit = temp.pop
+#     temp = num.split('')
+#     check_digit = temp.pop
 
-    if check_digit == 'x' || check_digit == 'X'
-        check_digit = 10
-    end
+#     if check_digit == 'x' || check_digit == 'X'
+#         check_digit = 10
+#     end
 
-    sum = 0
+#     sum = 0
 
-    temp.each_with_index do |v,i|
-        holder = v.to_i * (i + 1)
-        sum += holder
-    end
+#     temp.each_with_index do |v,i|
+#         holder = v.to_i * (i + 1)
+#         sum += holder
+#     end
 
-    checksum = sum % 11
+#     checksum = sum % 11
 
-    if checksum == check_digit.to_i
-        return true
-    else
-        return false
-    end
-end
+#     if checksum == check_digit.to_i
+#         return true
+#     else
+#         return false
+#     end
+# end
 
-def isbn_13(num)
-    num.gsub!(/[- ]/, '')
-    if num.gsub(/[\D]/, '') != num
-        return false
-    end
+# def isbn_13(num)
+#     num.gsub!(/[- ]/, '')
+#     if num.gsub(/[\D]/, '') != num
+#         return false
+#     end
 
-    temp = num.split('')
-    check_digit = temp.pop
+#     temp = num.split('')
+#     check_digit = temp.pop
 
-    sum = 0
-    count = 0
+#     sum = 0
+#     count = 0
 
-    temp.each do |v|
-        if count % 2 == 0
-            alt = 1
-        else
-            alt = 3
-        end
-        holder = v.to_i * alt
-        sum += holder
-        count += 1
-    end
+#     temp.each do |v|
+#         if count % 2 == 0
+#             alt = 1
+#         else
+#             alt = 3
+#         end
+#         holder = v.to_i * alt
+#         sum += holder
+#         count += 1
+#     end
     
-    checksum = 10 - (sum % 10)
-    if checksum.to_s.length > 1
-        checksum %= 10
-    end
+#     checksum = 10 - (sum % 10)
+#     if checksum.to_s.length > 1
+#         checksum %= 10
+#     end
 
-    if checksum == check_digit.to_i
-        return true
-    else
-        return false
-    end
-end
+#     if checksum == check_digit.to_i
+#         return true
+#     else
+#         return false
+#     end
+# end
 
-def isbn(num)
-    num.gsub!(/[- ]/, '')
-    if num.length == 10
-        return isbn_10(num)
-    elsif num.length == 13
-        return isbn_13(num)
-    else
-        return false
-    end
-end
+# def isbn(num)
+#     num.gsub!(/[- ]/, '')
+#     if num.length == 10
+#         return isbn_10(num)
+#     elsif num.length == 13
+#         return isbn_13(num)
+#     else
+#         return false
+#     end
+# end
 
 #Refactoring below
 
